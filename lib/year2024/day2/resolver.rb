5b69e5ecf60b8d5a2a9a971af6c5c7672bd2d177
immprada <im.mprada@gmail.com>
2024-12-05 11:21:13 -0500

    wip

--- lib/year2024/day2/resolver.rb
+++ lib/year2024/day2/resolver.rb
@@ -1,13 +1,60 @@
 module Year2024
   module Day2
     class Resolver
-      FILE_PATH = 'lib/year2024/day1/input.txt'.freeze
+      FILE_PATH = 'lib/year2024/day2/input.txt'.freeze
 
-      attr_reader :file_content
+      NEITHER_DECREASE_NOR_INCREASE = :none
+      INCREASE = :increase
+      DECREASE = :decrease
+      DIFF_LIMIT = 3
+
+      attr_reader :reports
 
       def initialize(path = nil)
         file_content = File.read(path || FILE_PATH)
-        @file_content = file_content
+        @reports = file_content.split("\n").map { |line| line.split.map(&:to_i) }
+      end
+
+      def report_safe?(report)
+        left_index = 0
+        right_index = report.size - 1
+
+        check_report(report, left_index, right_index)
+      end
+
+      private
+
+      def levels_direction(level1, level2)
+        return NEITHER_DECREASE_NOR_INCREASE if level1 == level2
+        return INCREASE if level1 < level2
+
+        DECREASE
+      end
+
+      def diff_magnitude(level1, level2)
+        (level1 - level2).abs
+      end
+
+      def diff_within_limit?(report, left_index, right_index)
+        diff_magnitude(
+          report[left_index],
+          report[left_index + 1]
+        ) <= DIFF_LIMIT && diff_magnitude(
+          report[right_index - 1],
+          report[right_index]
+        ) <= DIFF_LIMIT
+      end
+
+      def check_report(report, left_index, right_index, direction = nil)
+        left_value = report[left_index]
+        right_value = report[right_index]
+        border_direction = levels_direction(left_value, right_value)
+        return false if border_direction == NEITHER_DECREASE_NOR_INCREASE
+        return false unless diff_within_limit?(report, left_index, right_index)
+        return false if direction.present? && border_direction != direction
+        return true if left_index + 1 >= right_index
+
+        check_report(report, left_index + 1, right_index - 1, border_direction)
       end
     end
   end
--- spec/year2024/day2/resolver_spec.rb
+++ spec/year2024/day2/resolver_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
-RSpec.describe Year2024::Day2::Resolver do
-  subject(:resolver) { described_class.new('spec/year2024/day2/input.txt') }
-
+RSpec.describe Year2024::Day2::Resolver do # rubocop:disable RSpec/EmptyExampleGroup
+  # subject(:resolver) { described_class.new('spec/year2024/day2/input.txt') }
 end
