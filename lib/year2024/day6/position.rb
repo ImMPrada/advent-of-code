4f6fb564bfff876fb989f515de0037faa802c08b
immprada <im.mprada@gmail.com>
2024-12-09 16:48:08 -0500

    add Position to store `row`and `col` of map

---
+++ lib/year2024/day6/position.rb
@@ -0,0 +1,12 @@
+module Year2024
+  module Day6
+    class Position
+      attr_reader :row, :col
+
+      def initialize(row, col)
+        @row = row
+        @col = col
+      end
+    end
+  end
+end
