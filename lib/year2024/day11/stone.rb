f62a239533c6bb22798e8ddfcc97ee47eda93344
immprada <im.mprada@gmail.com>
2024-12-16 15:57:36 -0500

    add Stones class

--- lib/year2024/day11/stone.rb
+++ lib/year2024/day11/stone.rb
@@ -5,66 +5,23 @@ module Year2024
 
       def initialize(number)
         @number = number
-        @blinked = false
       end
 
-      def blink(stones_list)
-        run_rule1
-        run_rule2(stones_list)
-        run_rule3
-
-        @blinked = false
+      def rule1?
+        number.zero?
       end
 
-      def exists?
-        exists
-      end
-
-      private
-
-      attr_reader :blinked
-
-      def run_rule1
-        return if blinked
-        return if number != 0
-
+      def apply_rule1
         @number = 1
-        @blinked = true
       end
 
-      def run_rule2(stones_list)
-        return if blinked
-
+      def rule2?
         number_string = number.to_s
-        return unless number_string.size.even?
-
-        left_digits = number_string[0...number_string.size / 2]
-        right_digits = number_string[number_string.size / 2..]
-
-        update_stones_lists(stones_list, left_digits, right_digits)
-        @blinked = true
+        number_string.size.even?
       end
 
-      def run_rule3
-        return if blinked
-
+      def apply_rule3
         @number = number * 2024
-        @blinked = true
-      end
-
-      def update_stones_lists(stones_list, left_digits, right_digits)
-        backup_list = stones_list.dup
-        stones_list.clear
-
-        backup_list.each do |rock|
-          unless rock == self
-            stones_list << rock
-            next
-          end
-
-          stones_list << self.class.new(left_digits.to_i)
-          stones_list << self.class.new(right_digits.to_i)
-        end
       end
     end
   end
