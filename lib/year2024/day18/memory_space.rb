0eac794a72e595df822fa538abd175245a182b2b
immprada <im.mprada@gmail.com>
2024-12-27 14:12:24 -0500

    resolve part two

--- lib/year2024/day18/memory_space.rb
+++ lib/year2024/day18/memory_space.rb
@@ -1,9 +1,13 @@
 require 'matrix'
+require 'fileutils'
 require_relative 'cell'
+require_relative 'renderable'
 
 module Year2024
   module Day18
     class MemorySpace
+      include Renderable
+
       def initialize(size:)
         @size = size
         @grid = Matrix.build(size, size) do |row, col|
@@ -15,18 +19,18 @@ module Year2024
         @grid[y_coord, x_coord].corrupt!
       end
 
-      def find_shortest_path
+      def find_shortest_path(visualize: false)
         start = [0, 0]
         target = [@size - 1, @size - 1]
 
-        queue = [[start, 0]]
+        queue = [[start, [start]]] # [position, path]
         visited = Set.new([start])
 
         while queue.any?
-          position, steps = queue.shift
+          position, path = queue.shift
           x, y = position
 
-          return steps if position == target
+          return path.size - 1 if position == target
 
           neighbors(x, y).each do |next_x, next_y|
             next_pos = [next_x, next_y]
@@ -34,26 +38,12 @@ module Year2024
             next if visited.include?(next_pos)
 
             visited.add(next_pos)
-            queue << [next_pos, steps + 1]
+            queue << [next_pos, path + [next_pos]]
           end
         end
 
+        render(nil, title: 'no_path_found') if visualize
         nil
-      end
-
-      def visualize(path = nil)
-        puts "\nMemory Space:"
-        @grid.to_a.each_with_index do |row, y|
-          row.each_with_index do |cell, x|
-            if path&.include?([x, y])
-              print 'O'
-            else
-              print cell
-            end
-          end
-          puts
-        end
-        puts
       end
 
       private
--- lib/year2024/day18/resolver.rb
+++ lib/year2024/day18/resolver.rb
@@ -1,4 +1,5 @@
 require_relative 'memory_space'
+require 'fileutils'
 
 module Year2024
   module Day18
@@ -11,14 +12,27 @@ module Year2024
         @bytes = parse_bytes(lines)
       end
 
-      def run_case1
+      def run_case1(visualize: false)
         memory = MemorySpace.new(size: 71)
 
         @bytes.take(1024).each do |x, y|
           memory.corrupt(x, y)
         end
 
-        memory.find_shortest_path
+        memory.find_shortest_path(visualize:)
+      end
+
+      def run_case2(visualize: false)
+        memory = MemorySpace.new(size: 71)
+
+        @bytes.each_with_index do |(x, y), _index|
+          memory.corrupt(x, y)
+          return "#{x},#{y}" if memory.find_shortest_path(visualize:).nil?
+
+          memory.render(nil, title: "blocking_at_#{x}_#{y}")
+        end
+
+        nil
       end
 
       private
